1f6544902890f82d902fcf9720070dd0909682a0
Andrii <andrii.stotskyi.u@gmail.com>
2023-05-30 13:23:09 +0300

    completed lab 07

---
+++ LaboratoryWork7/src/TaxiServiceHelper.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+class TaxiServiceHelper {
+    public static Car getRandomCar(ArrayList<Car> cars) {
+        Random random = new Random();
+        int randomIndex = random.nextInt(cars.size());
+        return cars.get(randomIndex);
+    }
+}
